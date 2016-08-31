d446799afbc458774a897628d14d81e8389f01e7
Alexander Karlsson <alexander.karlsson@qlik.com>
2016-08-31 13:07:34 +0200

    explicitly serving main

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var open = require('open');
 var app = express();
 app.use(express.static(__dirname));
 
+app.use("/main", function(req, res){  
+  res.sendFile(__dirname + "/main.html");
+});
+
 app.listen(8000, function(){
   console.log('listening on port 8000');
 
